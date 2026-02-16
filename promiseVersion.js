e2a3aaa3b2af7220169f3811843799522115da6f
sheyla diaz <Dsheyla13@gmail.com>
2026-02-16 13:30:38 -0500

    update callback

---
+++ promiseVersion.js
@@ -0,0 +1,63 @@
+function mowYard(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`${name} mowed the yard.`);
+      resolve();
+    }, 2000);
+  });
+}
+
+function weedEat(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`${name} finished using weed eater.`);
+      resolve();
+    }, 1500);
+  });
+}
+
+
+function trimHedges(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`${name} finished trimming the edges.`);
+      resolve();
+    }, 1000);
+  });
+}
+
+
+function collectWood(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`${name} finished collecting wood.`);
+      resolve();
+    }, 2500);
+  });
+}
+
+
+function waterGarden(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`${name} finish watering the garden.`);
+      resolve();
+    }, 500);
+  });
+}
+
+function doSummerChores(name) {
+  mowYard(name)
+    .then(() => weedEat(name))
+    .then(() => trimHedges(name))
+    .then(() => collectWood(name))
+    .then(() => waterGarden(name))
+    .then(() => {
+      console.log(`${name} finished all their chores! 🎉`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+doSummerChores("Luna");
