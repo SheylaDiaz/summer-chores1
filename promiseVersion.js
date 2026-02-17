fb38d29509c83f3f0cb8d993f1ef8feb635d07a1
sheyla diaz <Dsheyla13@gmail.com>
2026-02-17 07:50:30 -0500

    update promiseversion.js

--- promiseVersion.js
+++ promiseVersion.js
@@ -8,52 +8,73 @@ function mowYard(name) {
 }
 
 function weedEat(name) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(`${name} finished using weed eater.`);
-      resolve();
+      let tiredness = Math.random();
+
+      if (tiredness > 0.7) {
+        reject(`${name} fell asleep after mowing the yard.`);
+      } else {
+        console.log(`${name} finished using weed eater.`);
+        resolve();
+      }
     }, 1500);
   });
 }
 
-
 function trimHedges(name) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(`${name} finished trimming the edges.`);
-      resolve();
+      let tiredness = Math.random();
+
+      if (tiredness > 0.6) {
+        reject(`${name} fell asleep after weed eating the yard.`);
+      } else {
+        console.log(`${name} finished trimming the edges.`);
+        resolve();
+      }
     }, 1000);
   });
 }
 
-
 function collectWood(name) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(`${name} finished collecting wood.`);
-      resolve();
+      let tiredness = Math.random();
+
+      if (tiredness > 0.5) {
+        reject(`${name} fell asleep after trimming the hedges.`);
+      } else {
+        console.log(`${name} collected wood.`);
+        resolve();
+      }
     }, 2500);
   });
 }
 
-
 function waterGarden(name) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(`${name} finish watering the garden.`);
-      resolve();
+      let tiredness = Math.random();
+
+      if (tiredness > 0.4) {
+        reject(`${name} fell asleep after collecting wood.`);
+      } else {
+        console.log(`${name} finished watering the garden.`);
+        resolve();
+      }
     }, 500);
   });
 }
 
 function doSummerChores(name) {
   mowYard(name)
-    .then(() => weedEat(name))
-    .then(() => trimHedges(name))
-    .then(() => collectWood(name))
-    .then(() => waterGarden(name))
+    .then(() => { console.log ("next: weedEat"); return weedEat(name);})
+    .then(() => { console.log ("next: trimHedges"); return trimHedges(name);}) 
+    .then(() => { console.log ("next: collectWood"); return collectWood(name);})
+    .then(() => { console.log ("next: waterGarden"); return waterGarden(name);})
     .then(() => {
-      console.log(`${name} finished all their chores! 🎉`);
+      console.log(`${name} finished all their chores!`);
     })
     .catch((error) => {
       console.log(error);
